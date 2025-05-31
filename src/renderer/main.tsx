fd4129fa1b17cc5b2d4d334350deb1807e26d47c
yarastqt <yarastqt@gmail.com>
2025-05-31 18:59:35 +0300

    chore: move sources to renderer

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,1 +1,26 @@
-console.log('>>> renderer')
+import { allSettled, fork } from 'effector'
+import { Provider } from 'effector-react'
+import { createRoot } from 'react-dom/client'
+import invariant from 'ts-invariant'
+
+import { Application } from '@app/application'
+import { GameModel } from '@app/shared/game'
+
+async function render() {
+  const rootElement = document.getElementById('app-root')
+
+  invariant(rootElement)
+
+  const root = createRoot(rootElement)
+  const scope = fork()
+
+  await allSettled(GameModel.createGame, { scope })
+
+  root.render(
+    <Provider value={scope}>
+      <Application />
+    </Provider>,
+  )
+}
+
+render()
